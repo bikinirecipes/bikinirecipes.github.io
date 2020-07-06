311d10855a724e50f364e0e41432557b297417b8
AdminA <admina@Cello.local>
2020-07-05 21:19:41 -0400

    update content

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+git checkout master
+git add .
+git commit -m 'update master'
+jekyll build
+rm -rf ~/_site
+mv _site ~
+git checkout gh-pages
+rm -rf *
+mv ~/_site/* .
+git add .
+git commit -m 'update content'
+git push https://github.com/bikinirecipes/bikinirecipes.github.io gh-pages:master
+git checkout master
+
